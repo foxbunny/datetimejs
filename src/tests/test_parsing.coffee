20397d12fdf4e080c3a8fb070762f4cbc3d9acb8
Branko Vukelic <branko@brankovukelic.com>
2013-09-02 12:14:56 +0200

    Reorganized tests and added a few tests for the parser

---
+++ src/tests/test_parsing.coffee
@@ -0,0 +1,54 @@
+# # Parser tests
+
+if require?
+  chai = require 'chai'
+  datetime = require '../datetime'
+
+if not GLOBAL?
+  root = this
+else
+  root = GLOBAL
+
+assert = chai.assert
+
+describe 'datetime.parse', () ->
+  describe '#strptime', () ->
+    it 'should return null when input is bogus', () ->
+      d = datetime.strptime 'bogus input', '%Y-%m-%d'
+      assert.equal d, null
+
+    it 'should parse full month name', () ->
+      for month, idx in datetime.MONTHS
+        d = datetime.strptime "#{month} 1 2013", '%B %D %Y'
+        assert.equal d.getMonth(), idx
+
+    it 'should parse full month case insensitively', () ->
+      for month, idx in datetime.MONTHS
+        d = datetime.strptime "#{month.toLowerCase()} 1 2013", '%B %D %Y'
+        assert.equal d.getMonth(), idx
+
+    it 'should parse short month name', () ->
+      for month, idx in datetime.MNTH
+        d = datetime.strptime "#{month} 1 2013", '%b %D %Y'
+        assert.equal d.getMonth(), idx
+
+    it 'should parse short month names case insensitively', () ->
+      for month, idx in datetime.MNTH
+        d = datetime.strptime "#{month.toLowerCase()} 1 2013", '%b %D %Y'
+        assert.equal d.getMonth(), idx
+
+    it 'should parse decimal seconds', () ->
+      d = datetime.strptime '2013-12-01 12:00:01.12', '%Y-%m-%d %H:%M:%f'
+      assert.equal d.getSeconds(), 1
+      assert.equal d.getMilliseconds(), 120
+
+  describe '#isoparse()', () ->
+    it 'should parse date in ISO format', () ->
+      d = datetime.isoparse '2013-09-01T16:00:00.00'
+      # Compensate for local timezone
+      d.setMinutes d.getMinutes() + d.getTimezoneOffset()
+      assert.equal d.getFullYear(), 2013
+      assert.equal d.getMonth(), 8
+      assert.equal d.getHours(), 16
+      assert.equal d.getMinutes(), 0
+
--- tests/test_formatting.js
+++ tests/test_formatting.js
@@ -12,97 +12,106 @@ d = new Date(2013, 8, 1, 18, 7, 8, 200);
 
 d1 = new Date(2013, 8, 1, 8, 7, 8, 200);
 
-describe('datetime.strftime', function() {
-  it('should return the short week day name', function() {
-    return assert.equal(datetime.strftime(d, '%a'), 'Sun');
+describe('datetime.format', function() {
+  describe('strftime()', function() {
+    it('should return the short week day name', function() {
+      return assert.equal(datetime.strftime(d, '%a'), 'Sun');
+    });
+    it('should return the long week day name', function() {
+      return assert.equal(datetime.strftime(d, '%A'), 'Sunday');
+    });
+    it('should return the short month name', function() {
+      return assert.equal(datetime.strftime(d, '%b'), 'Sep');
+    });
+    it('should return the long month name', function() {
+      return assert.equal(datetime.strftime(d, '%B'), 'September');
+    });
+    it('should return the zero-padded date', function() {
+      return assert.equal(datetime.strftime(d, '%d'), '01');
+    });
+    it('should return the non-padded date', function() {
+      return assert.equal(datetime.strftime(d, '%D'), '1');
+    });
+    it('should return zero-padded seconds with fraction', function() {
+      return assert.equal(datetime.strftime(d, '%f'), '08.20');
+    });
+    it('should return zero-padded hours in 24-hour format', function() {
+      return assert.equal(datetime.strftime(d, '%H'), '18');
+    });
+    it('should return non-padded hours in 12-hour format', function() {
+      return assert.equal(datetime.strftime(d, '%i'), '6');
+    });
+    it('should return zero-padded hours in 12-hour format', function() {
+      return assert.equal(datetime.strftime(d, '%I'), '06');
+    });
+    it('should return zer-padded day of year', function() {
+      return assert.equal(datetime.strftime(d, '%j'), '244');
+    });
+    it('should return zero-padded numeric month', function() {
+      return assert.equal(datetime.strftime(d, '%m'), '09');
+    });
+    it('should return zero-padded minutes', function() {
+      return assert.equal(datetime.strftime(d, '%M'), '07');
+    });
+    it('should return non-padded month', function() {
+      return assert.equal(datetime.strftime(d, '%n'), '9');
+    });
+    it('should return non-padded minutes', function() {
+      return assert.equal(datetime.strftime(d, '%N'), '7');
+    });
+    it('should return PM', function() {
+      return assert.equal(datetime.strftime(d, '%p'), 'p.m.');
+    });
+    it('should return AM', function() {
+      d.setHours(9);
+      assert.equal(datetime.strftime(d, '%p'), 'a.m.');
+      return d.setHours(18);
+    });
+    it('should return non-padded seconds', function() {
+      return assert.equal(datetime.strftime(d, '%s'), '8');
+    });
+    it('should return padded seconds', function() {
+      return assert.equal(datetime.strftime(d, '%S'), '08');
+    });
+    it('should return miliseconds', function() {
+      return assert.equal(datetime.strftime(d, '%r'), '200');
+    });
+    it('should return numeric weed day', function() {
+      return assert.equal(datetime.strftime(d, '%w'), '0');
+    });
+    it('should return year without century', function() {
+      return assert.equal(datetime.strftime(d, '%y'), '13');
+    });
+    it('should return full year', function() {
+      return assert.equal(datetime.strftime(d, '%Y'), '2013');
+    });
+    it('should return timezone', function() {
+      var original;
+      original = d.getTimezoneOffset;
+      d.getTimezoneOffset = function() {
+        return -120;
+      };
+      assert.equal(datetime.strftime(d, '%z'), '+0200');
+      d.getTimezoneOffset = function() {
+        return 360;
+      };
+      assert.equal(datetime.strftime(d, '%z'), '-0600');
+      return d.getTimezoneOffset = original;
+    });
+    it('should return literal percent', function() {
+      return assert.equal(datetime.strftime(d, '%%'), '%');
+    });
+    return it('should retain non-formatting character', function() {
+      var f;
+      f = datetime.strftime(d, 'The year is %Y, around %i %p on %B %D');
+      return assert.equal(f, "The year is 2013, around 6 p.m. on September 1");
+    });
   });
-  it('should return the long week day name', function() {
-    return assert.equal(datetime.strftime(d, '%A'), 'Sunday');
-  });
-  it('should return the short month name', function() {
-    return assert.equal(datetime.strftime(d, '%b'), 'Sep');
-  });
-  it('should return the long month name', function() {
-    return assert.equal(datetime.strftime(d, '%B'), 'September');
-  });
-  it('should return the zero-padded date', function() {
-    return assert.equal(datetime.strftime(d, '%d'), '01');
-  });
-  it('should return the non-padded date', function() {
-    return assert.equal(datetime.strftime(d, '%D'), '1');
-  });
-  it('should return zero-padded seconds with fraction', function() {
-    return assert.equal(datetime.strftime(d, '%f'), '08.20');
-  });
-  it('should return zero-padded hours in 24-hour format', function() {
-    return assert.equal(datetime.strftime(d, '%H'), '18');
-  });
-  it('should return non-padded hours in 12-hour format', function() {
-    return assert.equal(datetime.strftime(d, '%i'), '6');
-  });
-  it('should return zero-padded hours in 12-hour format', function() {
-    return assert.equal(datetime.strftime(d, '%I'), '06');
-  });
-  it('should return zer-padded day of year', function() {
-    return assert.equal(datetime.strftime(d, '%j'), '244');
-  });
-  it('should return zero-padded numeric month', function() {
-    return assert.equal(datetime.strftime(d, '%m'), '09');
-  });
-  it('should return zero-padded minutes', function() {
-    return assert.equal(datetime.strftime(d, '%M'), '07');
-  });
-  it('should return non-padded month', function() {
-    return assert.equal(datetime.strftime(d, '%n'), '9');
-  });
-  it('should return non-padded minutes', function() {
-    return assert.equal(datetime.strftime(d, '%N'), '7');
-  });
-  it('should return PM', function() {
-    return assert.equal(datetime.strftime(d, '%p'), 'p.m.');
-  });
-  it('should return AM', function() {
-    d.setHours(9);
-    assert.equal(datetime.strftime(d, '%p'), 'a.m.');
-    return d.setHours(18);
-  });
-  it('should return non-padded seconds', function() {
-    return assert.equal(datetime.strftime(d, '%s'), '8');
-  });
-  it('should return padded seconds', function() {
-    return assert.equal(datetime.strftime(d, '%S'), '08');
-  });
-  it('should return miliseconds', function() {
-    return assert.equal(datetime.strftime(d, '%r'), '200');
-  });
-  it('should return numeric weed day', function() {
-    return assert.equal(datetime.strftime(d, '%w'), '0');
-  });
-  it('should return year without century', function() {
-    return assert.equal(datetime.strftime(d, '%y'), '13');
-  });
-  it('should return full year', function() {
-    return assert.equal(datetime.strftime(d, '%Y'), '2013');
-  });
-  it('should return timezone', function() {
-    var original;
-    original = d.getTimezoneOffset;
-    d.getTimezoneOffset = function() {
-      return -120;
-    };
-    assert.equal(datetime.strftime(d, '%z'), '+0200');
-    d.getTimezoneOffset = function() {
-      return 360;
-    };
-    assert.equal(datetime.strftime(d, '%z'), '-0600');
-    return d.getTimezoneOffset = original;
-  });
-  it('should return literal percent', function() {
-    return assert.equal(datetime.strftime(d, '%%'), '%');
-  });
-  return it('should retain non-formatting character', function() {
-    var f;
-    f = datetime.strftime(d, 'The year is %Y, around %i %p on %B %D');
-    return assert.equal(f, "The year is 2013, around 6 p.m. on September 1");
+  return describe('#isoformat()', function() {
+    return it('should format date as ISO', function() {
+      d = new Date(2013, 8, 1, 16);
+      d.setMinutes(d.getMinutes() - d.getTimezoneOffset());
+      return assert.equal(datetime.isoformat(d), '2013-09-01T16:00:00.00');
+    });
   });
 });
